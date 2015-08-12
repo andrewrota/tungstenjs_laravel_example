e45fe9b2aa428a07459f5f52850e993454d9eac6
Matthew DeGennaro <mattdeeg@gmail.com>
2015-08-11 20:58:43 -0400

    Adding server-side Bootstrapper and client-side factory

--- resources/assets/javascript/app.js
+++ resources/assets/javascript/app.js
@@ -1,6 +1,13 @@
+var factory = require('./factory');
+
 var AppView = require('./views/todo_app_view');
 var AppModel = require('./models/todo_app_model');
 var template = require('../../views/todo_app_view.mustache');
+
+factory.registerView('AppView', AppView);
+factory.registerModel('AppModel', AppModel);
+factory.registerTemplate('todo_app_view', template);
+
 module.exports = new AppView({
   el: '#appwrapper',
   template: template,
